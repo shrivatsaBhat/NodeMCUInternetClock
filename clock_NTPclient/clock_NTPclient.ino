89491d734504a1e2ea1b9b7bf8be793746b20f0f
shrivatsaBhat <shrivatsabhatpolali@gmail.com>
2021-04-25 16:59:22 +0530

    multi wifi client and the error handle added

--- clock_NTPclient/clock_NTPclient.ino
+++ clock_NTPclient/clock_NTPclient.ino
@@ -6,12 +6,20 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <ESP8266WiFiMulti.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-const char *ssid     = "ssid";
-const char *password = "password";
+// Your WiFi network credentials
+const char *ssid1     = "ssid1";
+const char *password1 = "password1";
+const char *ssid2     = "ssid2";
+const char *password2 = "password2";
+
+// The URL we will use to test our connection
+const char* host = "www.google.co.in";
+const byte port = 80;
 
 const long utcOffsetInSeconds = 19800;     //UTC offset in seconds
 
@@ -19,9 +27,17 @@ char days[7][12]  = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
 char month[12][4] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);            //create object of NTP client
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds); //create object of NTP client
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);            //create object of ssd1306 -1 no hardware rest pin
+ESP8266WiFiMulti wifiMulti; // Create an instance of the ESP8266WiFiMulti class
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1); //create object of ssd1306 -1 no hardware rest pin
+
+// Function declarations
+bool ConnectWiFi(int);
+bool ConnectInternet(void);
+bool ConnectNetwork(void);
+void displayTimeDate(void);
 
 void setup()
 {
@@ -39,7 +55,6 @@ void setup()
   display.setTextSize(1);
   display.setTextColor(WHITE);
 
-  // display.setCursor(10,28);
   display.setCursor(3,28);
   display.setTextSize(2);
   display.print("PARDAKANDA");
@@ -47,32 +62,20 @@ void setup()
   delay(3500);
   display.clearDisplay();
 
-  
-  display.setCursor(3,28);
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-
   /*Connect to the WiFi*/
-  WiFi.begin(ssid, password);
-
-  display.print("Connecting");
-  while (WiFi.status() != WL_CONNECTED) 
-  {
-    delay(800);
-    //Serial.print (".");
-    display.print(".");
-    display.display();
-  }
-  display.setCursor(0,0);
-  display.setTextSize(1);
-  display.clearDisplay();
-  timeClient.begin();
+  WiFi.mode(WIFI_STA); // set-up a WPA2-PSK network
 }
+
+bool interGlobalState = false;
 
 void loop() 
 {
-  timeClient.update();          //upadte time
-  displayTimeDate();             //display time on OLED
+  bool status = ConnectNetwork();
+
+  if(status){
+    timeClient.update();          //upadte time
+    displayTimeDate();             //display time on OLED
+  }
   delay(100);
 }
 
@@ -80,7 +83,7 @@ void displayTimeDate()
 {
   String meridiem;
   int hour;
-  
+
   /*Calculate time and date*/
   unsigned long epochTime = timeClient.getEpochTime();
   struct tm *ptm = gmtime ((time_t *)&epochTime);
@@ -144,4 +147,87 @@ void displayTimeDate()
   display.setCursor(100,55);
   display.print(meridiem);
   display.display();
+}
+
+// @param int retryDelay: Time in milliseconds to wait between status checking (default 500ms)
+bool ConnectWiFi(int retryDelay = 500) {
+    // Add all available accesspoint names, ans passwords
+    wifiMulti.addAP(ssid1,password1);
+    wifiMulti.addAP(ssid2,password2);
+
+    if(WiFi.status() != WL_CONNECTED){
+        Serial.print("\nAttempting to connect to WiFi\n");
+        Serial.print("Connecting");
+        // Connect the strongest network
+        while(wifiMulti.run() != WL_CONNECTED){
+            Serial.print(".");
+            delay(retryDelay);
+        }
+        Serial.print("\nConnected to " + String(WiFi.SSID()) + "\n");
+        Serial.print("IP address:\t");
+        Serial.println(WiFi.localIP());
+        return true;
+    }
+    if(WiFi.status() == WL_CONNECTED) {
+        return true;
+    }
+    return false;
+}
+
+
+bool ConnectInternet() {
+    WiFiClient client;
+    // Connect to the host and port 
+    int isConnected = client.connect(host, port);
+
+    if (!isConnected){
+        Serial.println("Connection failed!!!");
+        client.stop();
+        delay(1000);
+        return false;
+    }
+    if(isConnected) {
+        Serial.println("Connection Built");
+        client.stop();
+        return true;
+    }
+    return false;
+}
+
+
+bool ConnectNetwork() {
+    // Holder to store Bolean status
+    bool hasInternet = false;
+    bool isWiFi = ConnectWiFi(800);
+    
+    if(!interGlobalState){
+      display.setCursor(3,28);
+      // display.setTextSize(2);
+      display.setTextColor(WHITE);
+
+      display.print("Connecting");
+      display.print("....");
+      display.display(); 
+    }
+
+    while(!isWiFi) {
+        isWiFi = ConnectWiFi(1000);
+    }
+    if(isWiFi){
+        hasInternet = ConnectInternet();
+        interGlobalState = hasInternet;
+
+        while(!hasInternet) {
+            hasInternet = ConnectInternet();
+        }
+
+        display.setCursor(0,0);
+        display.setTextSize(1);
+        display.clearDisplay();
+        timeClient.begin();
+        interGlobalState = hasInternet;
+        return hasInternet;
+    }
+    interGlobalState = hasInternet;
+    return hasInternet;
 }
